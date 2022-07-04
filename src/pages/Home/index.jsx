481256b54bf079e5f1866011f41d365d771a847c
Manuel Lino <mllino1@hotmail.com>
2022-07-04 03:24:13 +0100

    finished the App

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,26 +1,60 @@
+import React, { useState, useEffect } from "react";
+
 import "./styles.css";
 
-import {Card} from '../../components/Card'
+import { Card } from "../../components/Card";
 
 export function Home() {
+  const [studantName, setStudantName] = useState("");
 
-  let studantName ='';
-  function handleNameChange(name){
-    studantName = name
-    console.log(studantName)
+  const [studants, setStudants] = useState([]);
+
+  const [user, setUser] = useState({ name: "", avatar: "" });
+
+  function handlerStudant() {
+    console.log("btn was clicked");
+    const newStudant = {
+      name: studantName,
+      time: new Date().toLocaleDateString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
+    };
+    setStudants((prevState) => [...prevState, newStudant]);
   }
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/linox2000")
+      .then((response) => response.json())
+      .then((data) => {
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url,
+        });
+      });
+  }, []);
   return (
     <div className="container">
-      <h1>Nome: {studantName}</h1>
+      <header>
+        <h1>Lista de Prensenca</h1>
+        <div>
+          <strong>{user.name}</strong>
+        </div>
+        <img src={user.avatar} alt="foto de perfiel" />
+      </header>
       <input
-       type="text"
-       placeholder="Digite o nome.."
-       onChange={e=>handleNameChange(e.target.value)}
-        />
-      <button type="submit">Adicionar</button>
-      <Card name="manuelLino" time="10:55:23"/>
-      <Card name="LinoLino" time="20:15:53"/>
-      
+        type="text"
+        placeholder="Digite o nome.."
+        onChange={(e) => setStudantName(e.target.value)}
+      />
+      <button type="submit" onClick={handlerStudant}>
+        Adicionar
+      </button>
+
+      {studants.map((studant) => (
+        <Card key={studant.time} name={studant.name} time={studant.time} />
+      ))}
     </div>
   );
 }
